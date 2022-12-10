b7b86b598182cda74b5b16a79ea4ff4d7f4539b7
LaupWing <loc_strict@hotmail.com>
2022-12-10 15:05:15 +0100

    fixed the imports

--- 1_nft/contracts/Nft.sol
+++ 1_nft/contracts/Nft.sol
@@ -2,4 +2,6 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
