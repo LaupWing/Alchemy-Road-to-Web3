c22f6b71b5b7e6dadab700a72b32059d59cbd989
LaupWing <loc_strict@hotmail.com>
2022-12-10 16:01:36 +0100

    yeah

--- 1_nft/contracts/MyNft.sol
+++ 1_nft/contracts/MyNft.sol
@@ -6,12 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Alchemy is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Alchemy is ERC721, ERC721Enumerable, ERC721URIStorage {
     constructor() ERC721("Alchemy", "ALC") {}
 
     function safeMint(address to, uint256 tokenId, string memory uri)
         public
-        onlyOwner
     {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
