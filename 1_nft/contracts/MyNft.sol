3ecb0a49035dd44ef40d49c07630d8300bdbce28
LaupWing <loc_strict@hotmail.com>
2022-12-10 15:10:24 +0100

    hm

--- 1_nft/contracts/MyNft.sol
+++ 1_nft/contracts/MyNft.sol
@@ -1,28 +1,30 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNft is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable{
-   constructor() ERC721("MyNft", "MY") {}
+contract Alchemy is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor() ERC721("Alchemy", "ALC") {}
 
-   // function safeMint(address to, uint256 tokenId, string memory uri)
-   //    public
-   //    onlyOwner
-   // {
-   //    _safeMint(to, tokenId);
-   //    _setTokenURI(tokenId, uri);
-   // }
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 
-   function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-      internal
-      override(ERC721, ERC721Enumerable)
-   {
-      super._beforeTokenTransfer(from, to, tokenId);
-   }
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
