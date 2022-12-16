4770b377fb9e5c04d2e7353857441c9f42b9a07f
LaupWing <loc_strict@hotmail.com>
2022-12-16 16:24:12 +0100

    connectWallet

--- 2_BuyMeACoffee/frontend/pages/index.tsx
+++ 2_BuyMeACoffee/frontend/pages/index.tsx
@@ -28,16 +28,33 @@ export default function Home() {
    const isWalletConnected = async () => {
       try{
          const {ethereum} = window
-         const accounts = await ethereum?.request({method: "eth_accounts"}) as string[]
+         const accounts = await ethereum?.request<string[]>({method: "eth_accounts"})
          
          if(accounts!.length > 0) {
-            const account = accounts[0]
+            const account = accounts![0]
             console.log(`Wallet is connected ${account}`)
          } else {
             console.warn("Make sure Metamask is connected!")
          }
       }catch(e){
          console.error(`Error: ${e}`)
+      }
+   }
+
+   const connectWallet = async () => {
+      try {
+         const {ethereum} = window
+
+         if(!ethereum){
+            console.warn("Please install metamask")
+         }
+
+         const accounts = await ethereum?.request<string[]>({
+            method: "eth_requestAccounts"
+         })
+         setCurrentAccount(accounts![0] as string)
+      }catch(e) {
+         console.error(e)
       }
    }
 
