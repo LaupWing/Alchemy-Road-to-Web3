62b70104f3df7c759dca87d96c977e0cc28b6826
LaupWing <loc_strict@hotmail.com>
2022-12-16 16:10:13 +0100

    allright

--- 2_BuyMeACoffee/frontend/pages/index.tsx
+++ 2_BuyMeACoffee/frontend/pages/index.tsx
@@ -1,4 +1,39 @@
+import { useState } from "react"
+import {} from "ethers"
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+declare global {
+   interface Window {
+     ethereum?: MetaMaskInpageProvider;
+   }
+ }
+
 export default function Home() {
+   const contractAddress = "" // Need to import and create this one 
+   const contractAbi = {} // Need to import and create this one
+
+   const [currentAccount, setCurrentAccount] = useState("")
+   const [name, setName] = useState("")
+   const [message, setMessage] = useState("")
+   const [memo, setMemos] = useState("")
+
+   const onNameChange = (event: any) =>{
+      setName(event.target.name)
+   }
+
+   const onMessageChange = (event: any) =>{
+      setMessage(event.target.name)
+   }
+
+   const isWalletConnected = async () => {
+      try{
+         const {ethereum} = window
+
+      }catch(e){
+         
+      }
+   }
+
    return (
       <>
          
