4daeeaeed493428ad90f0e0f8a33c765e7658d02
LaupWing <loc_strict@hotmail.com>
2022-12-21 16:01:19 +0100

    base

--- 3_ChainBattles/hardhat.config.ts
+++ 3_ChainBattles/hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+   solidity: "0.8.17",
+   networks:{
+      mumbai:{
+         url: process.env.MUMBAI_MAINNET_RPC,
+         accounts: [process.env.PRIVATE_KEY as string],
+      }
+   },
+   etherscan: {
+      apiKey: process.env.POLYGON_API_KEY
+   }
+}
 
-export default config;
+export default config
