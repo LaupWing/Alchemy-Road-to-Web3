ab758e1cb0e9180c4ee7687036bab0e55d87cdf3
LaupWing <loc_strict@hotmail.com>
2022-12-22 16:01:00 +0100

    update

--- 3_ChainBattles/contracts/ChainBattles.sol
+++ 3_ChainBattles/contracts/ChainBattles.sol
@@ -18,7 +18,27 @@ contract ChainBattles is ERC721URIStorage {
 
    }
 
-   function generateCharater(uint256 tokenId) public returns (string memory){
-      
+   function generateCharater(uint256 tokenId) public view returns (string memory){
+      string memory svg = string.concat(
+         '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+         '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
+         '<rect width="100%" height="100%" fill="black" /',
+         '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
+         '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+         'Levels: ',
+         getLevels(tokenId),
+         '</text>',
+         '</svg>'
+      );
+
+      return string.concat(
+         "data:image/svg+xml;base64,",
+         Base64.encode(bytes(svg))
+      );
+   }
+
+   function getLevels(uint256 tokenId) public view returns(string memory){
+      uint256 levels = tokenIdToLevels[tokenId];
+      return levels.toString();
    }
 }
