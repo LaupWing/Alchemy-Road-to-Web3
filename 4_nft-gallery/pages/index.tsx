c04f8c5c0dbc8f7924f1cd364815af3b1d88fd80
LaupWing <loc_strict@hotmail.com>
2022-12-27 15:49:59 +0100

    update

--- 4_nft-gallery/pages/index.tsx
+++ 4_nft-gallery/pages/index.tsx
@@ -1,9 +1,30 @@
 import type { NextPage } from "next"
+import { useState } from "react"
 
 const Home: NextPage = () => {
+   const [wallet, setWallet] = useState("")
+
    return (
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
-         <div></div>
+         <div>
+            <input 
+               type="text" 
+               onChange={e => setWallet(e.target.value)}
+               placeholder="Add your wallet address"
+            />
+            <input 
+               type="text" 
+               placeholder="Add your collection address"
+            />
+            <label htmlFor="checkbox">
+               <input 
+                  type="checkbox" 
+                  name="checkbox" 
+                  id="checkbox" 
+               />
+            </label>
+            <button>Let's go!</button>
+         </div>
       </div>
    )
 }
